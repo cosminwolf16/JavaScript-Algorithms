436740a6e9f308c91b5874c25df2d5d4659511e4
Smokyn <smokyn_cosmin@yahoo.com>
2020-06-18 17:46:53 +0300

    recursion solution for factorial algo

--- math-algorithms/factorial.js
+++ math-algorithms/factorial.js
@@ -1,8 +1,19 @@
-function factorial(number) {
-  let result = 1;
-  for (let i = 2; i <= number; i++) {
-    result *= i;
+// function factorial(number) {
+//   let result = 1;
+//   for (let i = 2; i <= number; i++) {
+//     result *= i;
+//   }
+//   return result;
+// }
+
+function factorialRec(num) {
+  if (num <= 1) {
+    return 1;
   }
-  return result;
+  return num * factorialRec(num - 1);
 }
-console.log(factorial(5)); // 5 x 4 x 3 x 2 x1
+
+console.log(factorialRec(2)); //
+console.log(factorialRec(3)); //
+console.log(factorialRec(4)); //
+console.log(factorialRec(5)); //
