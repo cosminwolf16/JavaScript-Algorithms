8042ccc266404efb4e2882cce5068bf5da956b19
Smokyn <smokyn_cosmin@yahoo.com>
2020-06-18 11:52:27 +0300

    math algorithms mastered

--- math-algorithms/isPowerOfTwo.js
+++ math-algorithms/isPowerOfTwo.js
@@ -1,14 +1,31 @@
-function isPowerOfTwo(number) {}
+function isPowerOfTwo(number) {
+  let dividedNumber = number;
+  if (dividedNumber < 1) {
+    return false;
+  }
+  while (dividedNumber !== 1) {
+    if (dividedNumber % 2 !== 0) {
+      return false;
+    }
+    dividedNumber /= 2;
+  }
+  return true;
+}
 
-function isPowerOfTwo2(number) {}
+function isPowerOfTwo2(number) {
+  if (number < 1) {
+    return false;
+  }
+  return (number & (number - 1)) === 0;
+}
 
 // Best Case: number 13 => O(1)
 // Average Case: O(log n)
 // Worst Case: 1,125,899,906,842,624 =>  O(log n)
 
-console.log(isPowerOfTwo2(4));
-console.log(isPowerOfTwo2(8));
-console.log(isPowerOfTwo2(16));
-console.log(isPowerOfTwo2(64));
-console.log(isPowerOfTwo2(9));
-console.log(isPowerOfTwo2(10));
+console.log(isPowerOfTwo(4));
+console.log(isPowerOfTwo(8));
+console.log(isPowerOfTwo(16));
+console.log(isPowerOfTwo(64));
+console.log(isPowerOfTwo(9));
+console.log(isPowerOfTwo(10));
--- math-algorithms/problems.js
+++ math-algorithms/problems.js
@@ -1,10 +1,26 @@
 // The first algorithm should take an array of numbers as input and return the minimum value in the array (the smallest number)
-function getMin(list) {}
+function getMin(numbers) {
+  let currentMin = numbers[0];
+  for (const num of numbers) {
+    if (currentMin > num) {
+      currentMin = num;
+    }
+  }
+  return currentMin;
+}
 
 const arr = [15, 20, 33, 67, 2, 9, 100];
 // console.log(getMin(arr));
 
 // The second algorithm should take a number as input and return true if its and even number or false for odd number
-function isEven(n) {}
+function isEven(n) {
+  if (n % 2 === 0) {
+    return true;
+  }
+  return false;
+}
 
-console.log(isEven(2));
+// console.log(isEven(2));
+// console.log(isEven(3));
+// console.log(isEven(4));
+// console.log(isEven(5));
