fde6969835a84e038f6c2a99e3e153accac1e1c1
Smokyn <smokyn_cosmin@yahoo.com>
2020-06-18 18:03:20 +0300

    factorial dynammic programming + recursion

--- math-algorithms/factorial.js
+++ math-algorithms/factorial.js
@@ -13,7 +13,46 @@ function factorialRec(num) {
   return num * factorialRec(num - 1);
 }
 
-console.log(factorialRec(2)); //
-console.log(factorialRec(3)); //
-console.log(factorialRec(4)); //
-console.log(factorialRec(5)); //
+let counter = 0;
+function fib(n, memo) {
+  counter++;
+  let result;
+  if (memo[n]) {
+    return memo[n];
+  }
+  if (n === 0 || n === 1) {
+    result = 1;
+  } else {
+    result = fib(n - 1, memo) + fib(n - 2, memo);
+  }
+  memo[n] = result;
+  return result;
+}
+
+fib(2, {});
+console.log(counter);
+counter = 0;
+
+fib(3, {});
+console.log(counter);
+counter = 0;
+
+fib(4, {});
+console.log(counter);
+counter = 0;
+
+fib(5, {});
+console.log(counter);
+counter = 0;
+
+fib(6, {});
+console.log(counter);
+counter = 0;
+
+fib(20, {});
+console.log(counter);
+counter = 0;
+
+fib(30, {});
+console.log(counter);
+counter = 0;
