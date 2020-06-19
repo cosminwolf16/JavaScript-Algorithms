bc3a811d234b4583eff923b2a4b394f7ff65ce5b
Smokyn <smokyn_cosmin@yahoo.com>
2020-06-19 13:51:59 +0300

    sum and sumUpToNumber basic algos recap

--- getting-started/start.js
+++ getting-started/start.js
@@ -1,22 +1,19 @@
 const sumUp = (n) => {
   let result = 0;
-
   for (let i = 0; i <= n; i++) {
-    result += i;
+    result = result + i;
   }
-
   return result;
 };
 
-// const output1 = console.log(sumUp(2));
+const output1 = console.log(sumUp(5)); // 1 + 2 + 3 + 4 + 5 = 15
 
 const sumOfArray = (arr) => {
   let sum = 0;
-
   for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
+    sum = sum + arr[i];
   }
   return sum;
 };
 
-const output2 = console.log(sumOfArray([1, 2, 3, 4, 5]));
+// const output2 = console.log(sumOfArray([1, 2, 3]));
