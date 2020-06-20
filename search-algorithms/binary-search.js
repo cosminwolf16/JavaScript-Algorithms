91cb6c6392b2852da471465421fbc182b05b0bd7
Smokyn <smokyn_cosmin@yahoo.com>
2020-06-20 21:27:48 +0300

    binary-search recap

--- search-algorithms/binary-search.js
+++ search-algorithms/binary-search.js
@@ -1,4 +1,20 @@
-function findElement(sortedArr, element) {}
+function findElement(sortedArr, element) {
+  let startIndex = 0;
+  let endIndex = sortedArr.length - 1;
+
+  while (startIndex <= endIndex) {
+    const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+    if (sortedArr[middleIndex] === element) {
+      return middleIndex;
+    }
+    if (sortedArr[middleIndex] < element) {
+      startIndex = middleIndex + 1;
+    } else {
+      endIndex = middleIndex - 1;
+    }
+  }
+}
 
 const arr = [1, 5, 9, 13, 99, 53, 2, 3, 74, 100]; //
 
