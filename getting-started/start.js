07fff5e6a5bb3920700c747ba103cbd608193ec8
Smokyn <smokyn_cosmin@yahoo.com>
2020-06-20 19:11:53 +0300

    basic algorithms recap

--- getting-started/start.js
+++ getting-started/start.js
@@ -1,19 +1,19 @@
 const sumUp = (n) => {
   let result = 0;
   for (let i = 0; i <= n; i++) {
-    result = result + i;
+    result += i;
   }
   return result;
 };
 
-const output1 = console.log(sumUp(5)); // 1 + 2 + 3 + 4 + 5 = 15
+// const output1 = console.log(sumUp(5)); // 1 + 2 + 3 + 4 + 5 = 15
 
 const sumOfArray = (arr) => {
-  let sum = 0;
+  let result = 0;
   for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
+    result += arr[i];
   }
-  return sum;
+  return result;
 };
 
 // const output2 = console.log(sumOfArray([1, 2, 3]));
