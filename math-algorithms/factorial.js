83c84f945f36cc1ca0c9081f03ff25a41edc9121
Smokyn <smokyn_cosmin@yahoo.com>
2020-06-20 19:18:37 +0300

    factorial recap

--- math-algorithms/factorial.js
+++ math-algorithms/factorial.js
@@ -1,13 +1,13 @@
 function factorial(number) {
   let result = 1;
-  for (let i = number; i >= 2; i--) {
+  for (let i = 1; i <= number; i++) {
     result *= i;
   }
   return result;
 }
-// console.log(factorial(2));
-// console.log(factorial(3));
-// console.log(factorial(4));
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
 
 function factorialRec(num) {
   if (num <= 1) {
