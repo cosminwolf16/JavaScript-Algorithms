e838317de41e2ed0e87c7c27d11b06275dfe275f
Smokyn <smokyn_cosmin@yahoo.com>
2020-07-03 22:00:18 +0300

    sorting algorithms completed

--- sorting-algorithms/merge-sort.js
+++ sorting-algorithms/merge-sort.js
@@ -1,63 +1,40 @@
 function sort(arr) {
-  console.log('FUNCTION START');
-  console.log(arr);
-  // se verifica daca marimea listei e mai mica decat 2, daca da, se returneaza lista initiala.
+  // if the array's length is smaller than 2, return the array.
   if (arr.length < 2) {
     return arr;
   }
-  // se verifica daca lungimea listei este egala cu 2, si daca da, se returneaza lista sortata.
+  // if the array's length is equal to 2, sort the array from small to large
   if (arr.length === 2) {
-    console.log('EXACTLY 2 ITEMS');
-    console.log(arr[0] > arr[1] ? [arr[1], arr[0]] : arr);
     return arr[0] > arr[1] ? [arr[1], arr[0]] : arr;
   }
-  // se afla middle indexul listei si se creeaza lista 2 liste, left si right. Dupa care se sorteaza
+  // find middle index of the array + split the array into LeftArray and RightArray
   const middle = Math.floor(arr.length / 2);
-  const leftArray = arr.slice(0, middle);
-  const rightArray = arr.slice(middle);
-
-  const leftSortedArray = sort(leftArray);
-  const rightSortedArray = sort(rightArray);
-
-  console.log('AFTER RECURSIVE STEP');
-  console.log(leftSortedArray);
-  console.log(rightSortedArray);
-  // Se initializeaza arrayul final impreuna cu indexul 0 de la lista left si lista right.
-  const mergedArr = [];
-  let leftArrIndex = 0;
-  let rightArrIndex = 0;
-  // atata timp cat indexul stang e mai mic decat lungimea listei sortate left sau
-  // indexul drept e mai mic decat lungimea listei sortate right
-  // daca indexul listei left e mai mare sau egal cu lungimea listei sortate left sau
-  // daca indexul curent din lista sortata left e mai mare decat indexul curent din lista sortata right
-  // atunci se adauga in arrayul final indexul curent din lista sortata right
-  // daca nu se adauga in arryul final indexul curent din lista left
+  let leftArray = arr.slice(0, middle);
+  let rightArray = arr.slice(middle);
+  // after: sort both arrays and store them in new variables.
+  let leftSortedArray = sort(leftArray);
+  let rightSortedArray = sort(rightArray);
+  // init mergedArray and get/store starting index of both left and right list
+  const mergedArray = [];
+  let leftArrayIndex = 0;
+  let rightArrayIndex = 0;
+  // while left and right index are smaller than left and right list length
   while (
-    leftArrIndex < leftSortedArray.length ||
-    rightArrIndex < rightSortedArray.length
+    leftArrayIndex < leftSortedArray.length ||
+    rightArrayIndex < rightSortedArray.length
   ) {
     if (
-      leftArrIndex >= leftSortedArray.length ||
-      leftSortedArray[leftArrIndex] > rightSortedArray[rightArrIndex]
+      leftArrayIndex >= leftSortedArray.length ||
+      leftSortedArray[leftArrayIndex] > rightSortedArray[rightArrayIndex]
     ) {
-      mergedArr.push(rightSortedArray[rightArrIndex]);
-      rightArrIndex++;
+      mergedArray.push(rightSortedArray[rightArrayIndex]);
+      rightArrayIndex++;
     } else {
-      mergedArr.push(leftSortedArray[leftArrIndex]);
-      leftArrIndex++;
+      mergedArray.push(leftSortedArray[leftArrayIndex]);
+      leftArrayIndex++;
     }
   }
-
-  console.log('AFTER MERGE');
-  console.log(mergedArr);
-  // se returneaza arrayul final
-  return mergedArr;
+  return mergedArray;
 }
-
-// Recursive Step Time Complexity: O(n^logb(a)) => O(n^log2(2)) => O(n^1) => O(n)
-// Time Complexity outside of the Recursion: O(n)
-// Algorithm Time Complexity: O(n^logb(a) * log n) => O(n * log n)
-
-// const sortedArray = sort([-10, 33, 5, 10, 9, 3, -19, -99, 100]);
-const sortedArray = sort([-10, 33, 100, 5]);
+const sortedArray = sort([11, 10, 5, 2, 1, 7, 8, 9, 3, 4, 6]);
 console.log(sortedArray);
--- sorting-algorithms/quicksort.js
+++ sorting-algorithms/quicksort.js
@@ -1,35 +1,29 @@
 function sort(list) {
   const copiedList = [...list];
 
-  // Daca arrayul are 1 sau mai putine elemente, returneazal in starea initiala.
-  if (copiedList.length <= 1) {
+  if (copiedList.length < 2) {
     return copiedList;
   }
-  // Se initializeaza cele 3 arrayuri, small, big si middle numbers(cu pivotul in middle), plus
-  // elementul pivot extras primul din array
-  const smallElementsList = [];
-  const largeElementsList = [];
+
+  const smallElementsArray = [];
+  const largeElementsArray = [];
   const pivotElement = copiedList.shift();
-  const middleElementsList = [pivotElement];
-  // in timp ce arrayul contine ceva extrage elementul curent, adica primul din lista, si verifica
-  // daca elementul curent ii egal cu pivotul sa se duca in middle elements si daca ii mai mic in small elemenets, daca nu in large.
+  const middleElementsArray = [pivotElement];
+
   while (copiedList.length) {
     const currentElement = copiedList.shift();
     if (currentElement === pivotElement) {
-      middleElementsList.push(currentElement);
+      middleElementsArray.push(currentElement);
     } else if (currentElement < pivotElement) {
-      smallElementsList.push(currentElement);
+      smallElementsArray.push(currentElement);
     } else {
-      largeElementsList.push(currentElement);
+      largeElementsArray.push(currentElement);
     }
   }
-  // sorteaza lista small si lista mare dupa care returnezi lista mica impreuna cu middle si large
-  const sortedSmallElementsList = sort(smallElementsList);
-  const sortedLargeElementsList = sort(largeElementsList);
-  return sortedSmallElementsList.concat(
-    middleElementsList,
-    sortedLargeElementsList
-  );
+
+  const sortedSmallArray = sort(smallElementsArray);
+  const sortedLargeArray = sort(largeElementsArray);
+  return sortedSmallArray.concat(middleElementsArray, sortedLargeArray);
 }
 
 const output = sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
