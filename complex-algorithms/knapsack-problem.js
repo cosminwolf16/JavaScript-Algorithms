dac2b7a4f9621e5dd7ed7c8ad85d380e373cf3bb
Smokyn <smokyn_cosmin@yahoo.com>
2020-07-23 19:19:06 +0300

    adding memoization

--- complex-algorithms/knapsack-problem.js
+++ complex-algorithms/knapsack-problem.js
@@ -1,21 +1,28 @@
-function knapsack(items, cap, itemIndex) {
+function knapsackFn(items, cap, itemIndex, memo) {
+  console.log('RUNNING');
+  if (memo[cap][itemIndex]) {
+    return memo[cap][itemIndex];
+  }
   if (cap === 0 || itemIndex < 0) {
     return { items: [], value: 0, weight: 0 };
   }
   if (cap < items[itemIndex].weight) {
-    return knapsack(items, cap, itemIndex - 1);
+    return knapsackFn(items, cap, itemIndex - 1, memo);
   }
 
-  const sackWithItem = knapsack(
+  const sackWithItem = knapsackFn(
     items,
     cap - items[itemIndex].weight,
-    itemIndex - 1
+    itemIndex - 1,
+    memo
   );
 
-  const sackWithoutItem = knapsack(items, cap, itemIndex - 1);
+  const sackWithoutItem = knapsackFn(items, cap, itemIndex - 1, memo);
 
   const valueWithItem = sackWithItem.value + items[itemIndex].value;
   const valueWithoutItem = sackWithoutItem.value;
+
+  let resultSack;
 
   if (valueWithItem > valueWithoutItem) {
     const updatedSack = {
@@ -23,11 +30,23 @@ function knapsack(items, cap, itemIndex) {
       value: sackWithItem.value + items[itemIndex].value,
       weight: sackWithItem.weight + items[itemIndex].weight,
     };
-    return updatedSack;
+    resultSack = updatedSack;
   } else {
-    return sackWithoutItem;
+    resultSack = sackWithoutItem;
   }
+
+  memo[cap][itemIndex] = resultSack;
+
+  return resultSack;
 }
+
+function knapsack(items, cap, index) {
+  const mem = Array.from(Array(cap + 1), () =>
+    Array(items.length).fill(undefined)
+  );
+  return knapsackFn(items, cap, index, mem);
+}
+
 items = [
   { name: 'a', value: 3, weight: 3 },
   { name: 'b', value: 6, weight: 8 },
